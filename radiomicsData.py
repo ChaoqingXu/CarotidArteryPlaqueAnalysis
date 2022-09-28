36a3764863f38cbc514f995ba232363ca2ee8f0e
SuperClearXu <superclearXu@gmail.com>
2022-09-28 09:47:25 +0800

    split to feature cs

--- radiomicsData.py
+++ radiomicsData.py
@@ -14,22 +14,39 @@ import csv
 
 
 csvPath = r'D:\\OverleafProj\\CarotidArteryPlaqueAnalysis\\CarotidArteryData'
-# csvPath = r'D:\\OverleafProj\\CarotidArteryPlaqueAnalysis'
+csvSplitPath = r'D:\\OverleafProj\\CarotidArteryPlaqueAnalysis\\CarotidArteryData_SpilitLabels'
 
 dataRow = [ 'Image type',\
             'Feature Class',\
             'Feature Name',\
-            'Segmentation_segment_artery',\
-            'Segmentation_segment_capillary',\
-            'Segmentation_segment_fat',\
-            'Segmentation_segment_tissue']
+            'Segmentation_segment_Calcium',\
+            'Segmentation_segment_Fibrous',\
+            'Segmentation_segment_IPH',\
+            'Segmentation_segment_IPH_lipid']
 
-# read radiomics csv data and reorganize it as a standard format 
+
+## define function for writing to spilit csv files 
+def writeDataToSplitCSV(csvfile, featureID, segmentCalciumList, segmentFibrousList, segmentIPHlipidList, segmentIPHList):
+
+    print("write " + str(csvfile))
+    fileName = os.path.basename(csvfile).split('.')[0]
+
+    csvList = ['featureID','segmentArtery', 'segmentCapillary', 'segmentFat', 'segmentTissue']
+    labelList = [featureID, segmentCalciumList,segmentFibrousList, segmentIPHlipidList, segmentIPHList]
+
+    for i in range( len(csvList) ):
+        with open(csvSplitPath +"\\" + fileName + "_" + csvList[i] + ".csv", 'w', newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(labelList[i])
+        print("write " + fileName + " csv ")
+
+
+
+# read radiomics csv data, reorganize it as a standard format(correct the feature order), and write to split csv files
 def readData(csvPath):
     csvFiles = [f for f in listdir(csvPath) if isfile(join(csvPath, f))]
-    for f in csvFiles: 
-        print(f)
-        with open(csvPath +"\\" + f, 'r') as csvfile:
+    for fileName in csvFiles: 
+        with open(csvPath + "\\" + fileName, 'r') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             g = []
             for row in reader:
@@ -42,67 +59,59 @@ def readData(csvPath):
             for i in range(len( tmp[0])):
                 gRow.append(tmp[0][i])
             imageTypeList, featureClassList, featureNameList,\
-            segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList = [], [], [], [], [], [], []
+            segmentCalciumList, segmentFibrousList, segmentIPHlipidList, segmentIPHList = [], [], [], [], [], [], []
             for i in range(len(gRow)):
-                if gRow[i] == 'Image type':
-                    imageTypeList = g[:, [i]]
+                if gRow[i] == 'Image type': 
+                   imageTypeList = g[:, [i]]
                 if gRow[i] == 'Feature Class':
                     featureClassList = g[:, [i]]
                 if gRow[i] == 'Feature Name':
                     featureNameList = g[:, [i]]
-                if gRow[i] == 'Segmentation_segment_artery':
-                    segmentArteryList = g[:, [i]]
-                if gRow[i] == 'Segmentation_segment_capillary':
-                    segmentCapillaryList = g[:, [i]]
-                if gRow[i] == 'Segmentation_segment_fat':
-                    segmentFatList = g[:, [i]]
-                if gRow[i] == 'Segmentation_segment_tissue':
-                    segmentTissueList = g[:, [i]]
-            gFormat = np.hstack((imageTypeList, featureClassList, featureNameList,
-                        segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList))
+                if gRow[i] == 'Segmentation_segment_Calcium':
+                    segmentCalciumList = g[:, [i]]
+                if gRow[i] == 'Segmentation_segment_Fibrous':
+                    segmentFibrousList = g[:, [i]]
+                if gRow[i] == 'Segmentation_segment_IPH_lipid':
+                    segmentIPHlipidList = g[:, [i]]
+                if gRow[i] == 'Segmentation_segment_IPH':
+                    segmentIPHList = g[:, [i]]
+
+            # gFormat = np.hstack((imageTypeList, featureClassList, featureNameList,
+            #             segmentCalciumList, segmentFibrousList, segmentIPHlipidList, segmentIPHList))
         
             ## convert to split 4 labels and featureID
             imageTypeList_tmp, featureClassList_tmp, featureNameList_tmp = [], [], []
-            segmentArteryList_tmp, segmentCapillaryList_tmp, segmentFatList_tmp, segmentTissueList_tmp = [], [], [], []
-            for a, b, c, d, e, f, g in zip(imageTypeList, featureClassList, featureNameList, segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList):
+            segmentCalciumList_tmp, segmentFibrousList_tmp, segmentIPHlipidList_tmp, segmentIPHList_tmp = [], [], [], []
+            for a, b, c, d, e, f, g in zip(imageTypeList, featureClassList, featureNameList, segmentCalciumList, segmentFibrousList, segmentIPHlipidList, segmentIPHList):
                 imageTypeList_tmp.append(a[0])
                 featureClassList_tmp.append(b[0]) 
                 featureNameList_tmp.append(c[0])
-                segmentArteryList_tmp.append(d[0])
-                segmentCapillaryList_tmp.append(e[0])
-                segmentFatList_tmp.append(f[0])
-                segmentTissueList_tmp.append(g[0])
+                segmentCalciumList_tmp.append(d[0])
+                segmentFibrousList_tmp.append(e[0])
+                segmentIPHlipidList_tmp.append(f[0])
+                segmentIPHList_tmp.append(g[0])
 
             featureID = ({
-                'Image type': imageTypeList_tmp,
+                'Image Type': imageTypeList_tmp,
                 'Feature Class': featureClassList_tmp,
                 'Feature Name': featureNameList_tmp
             })
             df = pd.DataFrame(featureID)
-            df["featureID"] = df['Image type'] + "_" + df['Feature Class'] + "_" + df['Feature Name']
+            df["featureID"] = df['Image Type'] + "_" + df['Feature Class'] + "_" + df['Feature Name']
             featureID = df["featureID"].to_numpy()
-            segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList = segmentArteryList_tmp, segmentCapillaryList_tmp, segmentFatList_tmp, segmentTissueList_tmp
+            segmentCalciumList, segmentFibrousList, segmentIPHlipidList, segmentIPHList = segmentCalciumList_tmp, segmentFibrousList_tmp, segmentIPHlipidList_tmp, segmentIPHList_tmp
 
-            gFormat_FeautreID = np.vstack((featureID, segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList))
+            gFormat_FeautreID = np.vstack((featureID, segmentCalciumList, segmentFibrousList, segmentIPHlipidList, segmentIPHList))
             gFormat_FeautreID = gFormat_FeautreID.transpose()
 
-            return gFormat, gFormat_FeautreID, featureID, segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList
+            writeDataToSplitCSV(fileName, featureID, segmentCalciumList,
+                                segmentFibrousList, segmentIPHlipidList, segmentIPHList)
 
 
-def writeDataToSplitCSV(filename, featureID, segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList):
-
-    csvList = ['featureID','segmentArtery', 'segmentCapillary', 'segmentFat', 'segmentTissue']
-    labelList = [featureID, segmentArteryList,segmentCapillaryList, segmentFatList, segmentTissueList]
-
-    for i in range( len(csvList) ):
-        with open(csvPath + "\\CarotidArteryData_SpilitLabels\\" + filename + "_" + csvList[i] + ".csv", 'w', newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(labelList[i])
-        print("write" + csvList[i] +"csv ")
 
 
-gFormat, gFormat_FeautreID,\
-featureID, segmentArteryList, segmentCapillaryList, segmentFatList, segmentTissueList = readData(csvPath)
 
-# writeDataToSplitCSV('91307196_L',featureID, segmentArteryList,
-#                     segmentCapillaryList, segmentFatList, segmentTissueList)
+readData(csvPath)
+
+# writeDataToSplitCSV('91307196_L',featureID, segmentCalciumList,
+#                     segmentFibrousList, segmentIPHlipidList, segmentIPHList)
