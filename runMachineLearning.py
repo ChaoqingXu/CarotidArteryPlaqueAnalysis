d48889df5e4b4667eb24208f104bbe6d970deba5
SuperClearXu <superclearXu@gmail.com>
2022-10-16 14:46:48 +0800

    test classification

--- runMachineLearning.py
+++ runMachineLearning.py
@@ -101,29 +101,72 @@ def readHTMLCSVfile(HTMLCSVfile):
 HTMLplot_list, label_dictionary, subject_Category_List = readHTMLCSVfile(HTMLCSVfile)
 nsample, dim = np.shape(HTMLplot_list)
 plaque_Assignment = mat(zeros((nsample, 2)))
-y_plaque = []
 
+X = []
+for i in range( nsample ):
+    tmp = []
+    for j in range( dim ):
+        tmp.append(np.float64(HTMLplot_list[i][j]))
+    X.append(tmp)
+
+y = []
 for i in range(len(subject_Category_List)):
     if subject_Category_List[i].endswith('Calcium'):
         plaque_Assignment[i,:] = 0, 1
-        y_plaque.append(0)
+        y.append(0)
     if subject_Category_List[i].endswith('Fibrous'):
         plaque_Assignment[i, :] = 1, 1
-        y_plaque.append(1)
+        y.append(1)
     if subject_Category_List[i].endswith('IPH_lipid'):
         plaque_Assignment[i, :] = 2, 1
-        y_plaque.append(2)
+        y.append(2)
     if subject_Category_List[i].endswith('IPH'):
         plaque_Assignment[i, :] = 3, 1
-        y_plaque.append(3)
+        y.append(3)
 
-print(plaque_Assignment) 
-print(y_plaque)
+# print(plaque_Assignment) 
+print(y)
 
 
-# # shuffle and split training and test sets
+# # # shuffle and split training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 clf = OneVsRestClassifier( LinearSVC(random_state=0, dual=True, max_iter=100000) )
+
+y_score = clf.fit(X_train, y_train).decision_function(X_test)
+
+
+# n_classes = 4
+# # Compute ROC curve and ROC area for each class
+# fpr = dict()
+# tpr = dict()
+# roc_auc = dict()
+# for i in range(n_classes):
+#     fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+#     roc_auc[i] = auc(fpr[i], tpr[i])
+
+# # Compute micro-average ROC curve and ROC area
+# fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+# roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+# plt.figure()
+# lw = 2
+# plt.plot(
+#     fpr[2],
+#     tpr[2],
+#     color="darkorange",
+#     lw=lw,
+#     label="ROC curve (area = %0.2f)" % roc_auc[2],
+# )
+# plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel("False Positive Rate")
+# plt.ylabel("True Positive Rate")
+# plt.title("Receiver operating characteristic example")
+# plt.legend(loc="lower right")
+# plt.show()
+
+
 
 # # show clusters under DR
 # n_classes = len(AromaToLabel)
